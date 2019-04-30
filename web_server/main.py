de43e8d418d99e25dc9684a76a8f0802ba317e06
Roberto <robertorovella91@gmail.com>
2019-04-30 17:55:24 +0200

    ciao

--- web_server/main.py
+++ web_server/main.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('provahtml.html')
 
 def gen(camera,camera1):
     while True:
